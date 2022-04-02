70f3f3e19d8e309bbdc02e4281efb46d72491c05
junsu1220 <crab7157@gmail.com>
2022-04-02 18:50:32 +0900

    돌고돌아 git 설정 후 main에 commit, 개발환경, html, css

---
+++ src/js/keyboard.js
@@ -0,0 +1,20 @@
+export class Keyboard {
+  #swichEl;
+  constructor() {
+    this.#assignElement();
+    this.#addEvent();
+  }
+
+  #assignElement() {
+    this.#swichEl = document.getElementById("switch");
+  }
+
+  #addEvent() {
+    this.#swichEl.addEventListener("change", (event) => {
+      document.documentElement.setAttribute(
+        "theme",
+        event.target.checked ? "dark-mode" : ""
+      );
+    });
+  }
+}
